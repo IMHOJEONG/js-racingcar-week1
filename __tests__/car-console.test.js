c59acc74a489eedc8cd39abd49eed2c5ee23eea0
IMHOJEONG <39ghwjd@naver.com>
2025-02-09 00:31:34 +0900

    refactor: refactor test code

--- __tests__/car-console.test.js
+++ __tests__/car-console.test.js
@@ -5,6 +5,7 @@ import {
   makeCarObject,
   print,
   printWithCarName,
+  isForwardOverFour,
 } from "../src/console.js";
 
 jest.mock("readline");
@@ -12,13 +13,19 @@ jest.mock("readline");
 describe("콘솔 게임을 실행", () => {
   let readline;
   let read;
+  let randomSpy;
   beforeAll(async () => {
     readline = (await import("readline")).default;
     read = readline.createInterface();
+    randomSpy = jest.spyOn(Math, "random").mockReturnValue(0.5);
+  });
+
+  afterAll(() => {
+    randomSpy.mockRestore();
   });
 
   describe("초기 상태 : car Location - (0,0,0)", () => {
-    it("1. 자동차에 이름을 부여한다.", async () => {
+    test("1. 자동차에 이름을 부여한다.", async () => {
       const input = "경주할 자동차 이름을 입력하세요.";
       const carName = "car1";
       const expectedCarName = ["car1"];
@@ -26,17 +33,17 @@ describe("콘솔 게임을 실행", () => {
       const response = await read.question(input, carName);
       const actualCarName = makeToArray(response);
 
-      expect(expectedCarName).toEqual(actualCarName);
+      expect(actualCarName).toEqual(expectedCarName);
     });
 
-    it("1-1. 자동차 여러 대의 이름을 부여한다. - 쉼표를 기준", async () => {
+    test("1-1. 자동차 여러 대의 이름을 부여한다. - 쉼표를 기준", async () => {
       const input = "경주할 자동차 이름을 입력하세요.";
       const carName = "car1, car2, car3";
       const expectedCarName = ["car1", "car2", "car3"];
 
       const response = await read.question(input, carName);
       const actualCarName = makeToArray(response);
-      expect(expectedCarName).toEqual(actualCarName);
+      expect(actualCarName).toEqual(expectedCarName);
     });
 
     test("2-1. 자동차 여러 대의 이름을 부여한다. - 5자 이하만 가능", async () => {
@@ -46,7 +53,7 @@ describe("콘솔 게임을 실행", () => {
 
       const response = await read.question(input, carName);
       const actualResult = isNameLessThanFive(makeToArray(response));
-      expect(expectedResult).toBe(actualResult);
+      expect(actualResult).toBe(expectedResult);
     });
 
     test("3. 자동차 경주는 5회로 고정하여 진행", () => {
@@ -58,7 +65,7 @@ describe("콘솔 게임을 실행", () => {
       const actualGameCount = race(carObjs, 5).length;
 
       // then
-      expect(expectedGameCount).toBe(actualGameCount);
+      expect(actualGameCount).toBe(expectedGameCount);
     });
 
     test("4. 각 loop마다 자동차가 지나간 궤적을 기록", () => {
@@ -76,7 +83,7 @@ describe("콘솔 게임을 실행", () => {
       ]);
 
       // then
-      expect(expectedResult).toEqual(actualResult);
+      expect(actualResult).toEqual(expectedResult);
     });
 
     it("4-1. 전진하는 자동차를 출력 시, 자동차 이름을 같이 출력", async () => {
@@ -93,12 +100,28 @@ describe("콘솔 게임을 실행", () => {
     test("4-1. 자동차가 지나간 궤적을 출력", () => {
       // given
       const carObjs = makeCarObject([1, 2, 3, 4, 5], { x: 0, y: 0, z: 0 });
+      const expectedResultLength = 5;
+
       // when
       const actualResultLength = race(carObjs, 5)[0].join("").length;
 
       // then
-      expect(5).toBe(actualResultLength);
+      expect(actualResultLength).toBe(expectedResultLength);
     });
+
+    test("4-2. 전진하는 조건은 0에서 9 사이에서 무작위 값 중 4 이상인 경우", () => {
+      // given
+      randomSpy.mockReturnValue(0.8);
+      const carObjs = makeCarObject([1, 2, 3, 4, 5], { x: 0, y: 0, z: 0 });
+      const expectedResult = [true, true, true, true, true];
+
+      // when
+      const actualResult = carObjs.map(() => isForwardOverFour());
+
+      // then
+      expect(actualResult).toEqual(expectedResult);
+    });
+
     test("5. 사용자가 잘못된 이름을 작성한 경우, 프로그램을 종료", () => {
       // given
       expect(() => {
